4a8ce61474222417a3a62345c2c4bb4ea072f653
Charlie Shepherd <cs648@cam.ac.uk>
2013-08-23 14:44:10 +0100

    Set the correct permissions on the private key in fetch_image.sh

--- fetch_image.sh
+++ fetch_image.sh
@@ -2,3 +2,7 @@
 
 wget http://www.ctshepherd.com/testbedhdd.img.bz2
 bunzip2 testbedhdd.img.bz2
+
+# We need to set the correct permissions on the private key, as ssh doesn't
+# like it being open but git won't remember how the correct permissions.
+chmod 0600 cpc-test.rsa
