8611b3926703afb3fa92a9f26e64a7710df4ec06
Charlie Shepherd <charlie@ctshepherd.com>
2013-08-25 20:42:25 +0200

    Start QEMU with -nographic for headless hosts

--- start_guest.sh
+++ start_guest.sh
@@ -3,4 +3,4 @@
 QEMU_BIN=qemu
 IMAGE=testbedhdd.img
 
-$QEMU_BIN -hda $IMAGE -redir tcp:2222::22 -monitor stdio -name CPC-Test
+$QEMU_BIN -hda $IMAGE -redir tcp:2222::22 -monitor stdio -name CPC-Test -nographic
