affd72c29c10143bbca4cdb87e950ab89bca435a
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-05 16:48:03 +0200

    Fix build_host.sh

--- build_host.sh
+++ build_host.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh -e
 
 ROOTDIR=$(pwd)
 
@@ -20,9 +20,12 @@ cd bin/master
 ../../configure --disable-werror \
     --with-coroutine=ucontext    \
     --target-list=x86_64-softmmu \
-    --cc="$(ROOTDIR)/cpc/bin/cpc" \
-    --extra-cflags="--dont-cpc -U__SSE2__"
-make
+    --cc="$ROOTDIR/cpc/bin/cpc" \
+    --extra-cflags="--dontcpc -U__SSE2__ --save-temps -Wno-unused-variable -Wno-redundant-decls -Wno-deprecated-declarations"
+make -j8
+
+# CPC QEMU does not work yet
+exit 0
 
 # Build CPC QEMU
 cd ..
@@ -31,6 +34,6 @@ cd bin/cpc
 ../../configure --disable-werror \
     --with-coroutine=cpc         \
     --target-list=x86_64-softmmu \
-    --cc="$(ROOTDIR)/cpc/bin/cpc" \
-    --extra-cflags="-U__SSE2__"
-make
+    --cc="$ROOTDIR/cpc/bin/cpc" \
+    --extra-cflags="-U__SSE2__ --save-temps -Wno-unused-variable -Wno-redundant-decls -Wno-deprecated-declarations"
+make -j8
