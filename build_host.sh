c99818f25c9ad8f1dc45ab7f95cf3e3217ee3820
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-06 11:53:46 +0200

    More reliable and complete build script

--- build_host.sh
+++ build_host.sh
@@ -1,9 +1,51 @@
 #!/bin/sh -e
 
-ROOTDIR=$(pwd)
+# Use DONTCLEAN=1 ./build_host.sh to avoid rebuilding everything
+
+ROOTDIR="$(pwd)"
+
+CONFIGOPTS="--disable-werror --target-list=x86_64-softmmu"
+CILOPTS="--save-temps --noMakeStaticGlobal --useLogicalOperators \
+  --useCaseRange"
+GCCOPTS="-U__SSE2__ -Wno-unused-variable -Wno-redundant-decls \
+  -Wno-deprecated-declarations"
+COROOPTS="--load=$ROOTDIR/corocheck/_build/corocheck.cma \
+  --doCoroCheck --coopAttr=cps --blockAttr=nocps"
+
+CILBIN="$ROOTDIR/cil/bin/cilly"
+CPCBIN="$ROOTDIR/cpc/bin/cpc"
+
+# Make CIL
+cd cil
+if [ -z "$DONTCLEAN" ]; then
+  git clean -fdx
+fi
+git checkout develop
+git pull
+./configure
+make
+
+cd ..
+
+# Make CoroCheck
+cd corocheck
+if [ -z "$DONTCLEAN" ]; then
+  git clean -fdx
+fi
+git checkout master
+git pull
+# Force clean: fast to build, make sure its consistent with latest CIL
+make clean all OCAMLPATH=$ROOTDIR/cil/lib
+
+cd ..
 
 # Make CPC
 cd cpc
+if [ -z "$DONTCLEAN" ]; then
+  git clean -fdx
+fi
+git checkout develop
+git pull
 ./configure
 make
 
@@ -11,29 +53,71 @@ cd ..
 
 # Prepare QEMU
 cd qemu
-mkdir -p bin/master
-mkdir -p bin/cpc
+if [ -z "$DONTCLEAN" ]; then
+  git clean -fdx
+fi
+mkdir -p bin/gcc-ucontext
+mkdir -p bin/cil-ucontext
+mkdir -p bin/cpc-ucontext
+mkdir -p bin/cpc-cpc
 
-# Build vanilla QEMU
+# Build gcc-ucontext QEMU
+
 git checkout master
-cd bin/master
-../../configure --disable-werror \
+git pull
+
+cd bin/gcc-ucontext
+../../configure $CONFIGOPTS \
     --with-coroutine=ucontext    \
-    --target-list=x86_64-softmmu \
-    --cc="$ROOTDIR/cpc/bin/cpc" \
-    --extra-cflags="--dontcpc -U__SSE2__ --save-temps -Wno-unused-variable -Wno-redundant-decls -Wno-deprecated-declarations"
+    --extra-cflags="$GCCOPTS"
 make -j8
 
+cd ../..
+
+# Build cil-ucontext QEMU (with CoroCheck)
+
+git checkout master
+git pull
+
+cd bin/cil-ucontext
+../../configure $CONFIGOPTS \
+    --with-coroutine=ucontext    \
+    --cc="$CILBIN" \
+    --extra-cflags="$GCCOPTS $CILOPTS $COROOPTS" 
+make -j8
+
+cd ../..
+
+# Build  cpc-ucontext QEMU
+
+git checkout master
+git pull
+
+cd bin/cpc-ucontext
+../../configure $CONFIGOPTS \
+    --with-coroutine=ucontext    \
+    --cc="$CPCBIN" \
+    --extra-cflags="--dontcpc $GCCOPTS $CILOPTS"
+make -j8
+
+cd ../..
+
 # CPC QEMU does not work yet
+
+echo Everything built successfully!
+echo Skipping cpc-qemu.
 exit 0
 
 # Build CPC QEMU
-cd ..
+
 git checkout cpc
-cd bin/cpc
-../../configure --disable-werror \
-    --with-coroutine=cpc         \
-    --target-list=x86_64-softmmu \
-    --cc="$ROOTDIR/cpc/bin/cpc" \
-    --extra-cflags="-U__SSE2__ --save-temps -Wno-unused-variable -Wno-redundant-decls -Wno-deprecated-declarations"
+git pull
+
+cd bin/cpc-cpc
+../../configure $CONFIGOPTS \
+    --with-coroutine=cpc    \
+    --cc="$CPCBIN" \
+    --extra-cflags="$GCCOPTS $CILOPTS"
 make -j8
+
+cd ../..
--- fetch_resources.sh
+++ fetch_resources.sh
@@ -4,7 +4,9 @@ wget http://www.ctshepherd.com/testbedhdd.img.bz2
 bunzip2 testbedhdd.img.bz2
 
 git clone git://git.qemu-project.org/qemu.git
+git clone git://github.com/kerneis/cil
 git clone git://github.com/kerneis/cpc
+git clone git://github.com/kerneis/corocheck
 
 # We need to set the correct permissions on the private key, as ssh doesn't
 # like it being open but git won't remember how the correct permissions.
