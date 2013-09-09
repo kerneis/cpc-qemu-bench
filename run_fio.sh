5840d95911b4f03ee1cc9a4c528418ba79a39bf1
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-09 18:08:10 +0200

    Tweaks

--- run_fio.sh
+++ run_fio.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 rw=$1
 jobs=$2
@@ -6,29 +6,32 @@ backend=$3
 
 name=bench-$rw
 
-cmd="fio --minimal --name=$name --rw=$rw --bs=32k --runtime=30 --time_based --size=100m --numjobs=$jobs"
+cmd="fio --minimal --name=$name --rw=$rw --bs=32k --runtime=30 --ramp_time=15 --time_based --size=100m --numjobs=$jobs"
 logdir=$(date +data/%Y/%m/%d)
-log=${logdir}/${name}-${backend}-$(date +%H%M%S).log
+log=${logdir}/${name}-${backend}-${jobs}-$(date +%H%M%S).log
 
 echo Pre-cleaning
 rm -f $log
 rm -f bench-*
-./run_guest.sh find . -type f -name 'bench-*' -delete
-./run_guest.sh find . -type f -name 'bench-*' -delete
+./run_guest.sh rm -f 'bench-*'
+
+mkdir -p $logdir
 
 echo LOGFILE IS: $log
 echo ==========================================
 
 uname -a | tee -a $log
 file testbedhdd.img | tee -a $log
-echo Guest backend: $backend | tee -a $log
+echo backend: $backend | tee -a $log
 echo $cmd | tee -a $log
+echo "(adding --direct=1 for guest)"
+echo
 echo "Host:" | tee -a $log
 $cmd | tee -a $log
+echo
 echo "Guest:" | tee -a $log
 ./run_guest.sh $cmd --direct=1 | tee -a $log
 
 echo Post-cleaning
 rm -f bench-*
-./run_guest.sh find . -type f -name 'bench-*' -delete
-./run_guest.sh find . -type f -name 'bench-*' -delete
+./run_guest.sh rm -f 'bench-*'
