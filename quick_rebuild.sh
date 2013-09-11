5cdc8342252784e54c7f6c1dc3cf6e8e4a5ca36f
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-11 09:51:38 +0200

    quick_rebuild can rebuild nopool now

--- quick_rebuild.sh
+++ quick_rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-for dir in $(ls qemu/bin|grep -v nopool); do
+for dir in $(ls qemu/bin); do
   echo ========== $dir ===========
   # fix config-host.mak if necessary
   sed -i 's/^extra_cflags=\([^"].*\)$/extra_cflags="\1"/' "qemu/bin/${dir}/config-host.mak"
