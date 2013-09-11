1ddd889ef67a64612dca7ae1fd61890c8759b576
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-11 18:15:53 +0200

    Script for full benchmark

---
+++ full_bench.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Do NOT add -e for this file! We want to carry on if one backend fails
+
+logfile=benchsuite.log
+
+rm -f $logfile
+
+for backend in $(ls qemu/bin); do
+  ./run_bench.sh $backend >> $logfile 2>&1
+  sleep 30
+done
