cf8e6811174cfceceb53afb62db146985872da72
Gabriel Kerneis <gabriel@kerneis.info>
2013-09-19 18:16:33 +0200

    Compute microbench stats

--- plot/microbench.R
+++ plot/microbench.R
@@ -3,13 +3,21 @@
 data <- read.csv("../data/microbench.csv")
 
 # aggregate by backend
-mean_agg <- aggregate(time ~ backend, FUN="mean", data=data)
-median_agg <- aggregate(time ~ backend, FUN="median", data=data)
+mean_agg <- aggregate(list(time = round(data$time * 1e9 / data$iterations)),
+                      by=list(backend = sapply(data$backend, function(x) gsub("-nopool", "", x)),
+                              pool = sapply(data$backend, function(x) toString(!grep("-nopool", x))),
+                              test = data$test),
+                      FUN="mean")
+median_agg <- aggregate(list(time = round(data$time * 1e9 / data$iterations)),
+                      by=list(backend = sapply(data$backend, function(x) gsub("-nopool", "", x)),
+                              pool = sapply(data$backend, function(x) toString(!grep("-nopool", x))),
+                              test = data$test),
+                      FUN="median")
 
 # sort
 
 print("Mean time (over 10 runs)")
-mean_agg[with(mean_agg, order(time,backend)),]
+mean_agg[with(mean_agg, order(test, pool, time,backend)),]
 
 print("Median time (over 10 runs)")
-median_agg[with(median_agg, order(time,backend)),]
+median_agg[with(mean_agg, order(test, pool, time,backend)),]
